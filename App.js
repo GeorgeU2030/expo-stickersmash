946838e897374194b9231e47762512feec832934
GeorgeU2030 <georgeu2021@gmail.com>
2024-02-18 09:41:32 -0500

    button_and_font

--- App.js
+++ App.js
@@ -1,21 +1,26 @@
 import { StatusBar } from 'expo-status-bar';
 import { StyleSheet, View } from 'react-native';
 
- import ImageViewer from './components/ImageViewer'; 
+import ImageViewer from './components/ImageViewer'; 
+import Button from './components/Button';
 
 const PlaceholderImage = require('./assets/images/background-image.png');
 
 export default function App() {
+  
   return (
     <View style={styles.container}>
       <View style={styles.imageContainer}>
-        <ImageViewer placeholderImageSource={PlaceholderImage} />
+      <ImageViewer placeholderImageSource={PlaceholderImage} />
+      </View>
+      <View style={styles.footerContainer}>
+      <Button theme="primary" label="Choose a photo" />
+      <Button label="Use this photo" />
       </View>
       <StatusBar style="auto" />
     </View>
   );
 }
-
 
 const styles = StyleSheet.create({
   container: {
@@ -26,6 +31,10 @@ const styles = StyleSheet.create({
   imageContainer: {
     flex: 1,
     paddingTop: 58,
+  },
+  footerContainer: {
+    flex: 1 / 3,
+    alignItems: 'center',
   },
 });
 
